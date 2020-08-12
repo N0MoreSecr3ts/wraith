9b8b03422aaac5d4e95850036caab274b6437fb5
Matty Jones <urlugal@gmail.com>
2020-08-12 18:41:34 -0400

    use var

--- cmd/scanLocalGitRepo.go
+++ cmd/scanLocalGitRepo.go
@@ -32,7 +32,7 @@ var scanLocalGitRepoCmd = &cobra.Command{
 		//sess.Out.Info("%s\n\n", common.ASCIIBanner)
 		sess.Out.Important("%s v%s started at %s\n", core.Name, version.AppVersion(), sess.Stats.StartedAt.Format(time.RFC3339))
 		sess.Out.Important("Loaded %d signatures.\n", len(core.Signatures))
-		sess.Out.Important("Web interface available at http://%s:%d\n", sess.BindAddress, sess.BindPort)
+		sess.Out.Important("Web interface available at http://%s:%d\n", "127.0.0.1", 9393)
 
 		core.GatherLocalRepositories(sess)
 		core.AnalyzeRepositories(sess)
